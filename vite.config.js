103fb93eb8c95683a3dde39876d5a5ab00f6eccf
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-07-17 00:50:59 +0200

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/vite-react/' : './',
-  build: {
-    outDir: 'dist',
-    assetsDir: './',
-    rollupOptions: {
-      input: './src/main.jsx',
-    },
-  },
-})
+  base:"/vite-react/"
+});
