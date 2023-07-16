3b0de2bdcda09c9b7d843942d54cfd3da95aa0da
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-07-17 00:16:50 +0200

    wip

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: '/vite-react/',
+    base: '/',
   }
 
   if (command !== 'serve') {
