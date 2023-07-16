92ecf722ce4182859dc5e4ff790e10cc85ef813d
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-07-17 00:32:36 +0200

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/vite-react/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/vite-react/' : './',
+  build: {
+    outDir: 'dist',
+    assetsDir: './',
+    rollupOptions: {
+      input: './src/main.jsx',
+    },
+  },
 })
