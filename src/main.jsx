ec963c8fe5edbd841485fc8e4900b6318fa9a209
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-07-17 16:57:03 +0200

    rename folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App/App.jsx';
+import App from './assets/App/App.jsx';
 import './main.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
